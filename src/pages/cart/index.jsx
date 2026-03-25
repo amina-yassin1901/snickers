38194bbb2cee91eb9d569357a277b570961acc93
AminaYassin <amina6012016@gmail.com>
2026-03-25 21:43:05 +0100

    cart done

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,2 +1,9 @@
-function Cart() {}
+import CartComponent from "../../components/cartComponent";
+function Cart() {
+  return (
+    <div>
+      <CartComponent />
+    </div>
+  );
+}
 export default Cart;
